10725e23a12c76e7bd39bbe32238afac6dc0f609
1dev-Ebrahim <ebrahim.mohammed.dev@gmail.com>
2025-06-24 18:21:32 +0300

    Refactor app structure: replace HomeScreen with HomeView and setting the theme

--- lib/notes_app.dart
+++ lib/notes_app.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/screens/home_screen.dart';
+import 'package:notes_app/views/home_view.dart';
 
 class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: HomeScreen());
+    return MaterialApp(
+      home: HomeView.HomeView(),
+      theme: ThemeData(brightness: Brightness.dark),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
