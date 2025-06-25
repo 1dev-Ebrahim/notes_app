624daf6ce559e326266d22c96754fed8fc67aa82
1dev-Ebrahim <ebrahim.mohammed.dev@gmail.com>
2025-06-26 01:54:32 +0300

    Enhance AddNoteBottomSheet: implement UI with title input fields and improved layout

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -14,6 +14,7 @@ class HomeView extends StatelessWidget {
           showModalBottomSheet(
             context: context,
             builder: (context) => const AddNoteBottomSheet(),
+            showDragHandle: true,
           );
         },
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -5,6 +5,44 @@ class AddNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        TextField(
+          decoration: InputDecoration(
+            hintText: 'Title',
+            hintStyle: TextStyle(color: Colors.blue.shade300),
+            enabledBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.white),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.white),
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+        const SizedBox(height: 20),
+        SizedBox(
+          height: 200,
+          child: TextField(
+            expands: true,
+            maxLines: null,
+            minLines: null,
+            decoration: InputDecoration(
+              hintText: 'Title',
+              hintStyle: TextStyle(color: Colors.blue.shade300),
+              enabledBorder: OutlineInputBorder(
+                borderSide: const BorderSide(color: Colors.white),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: const BorderSide(color: Colors.white),
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
