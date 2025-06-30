622cc5376a5d179b708d60bf7c34a20cea1b9911
1dev-Ebrahim <ebrahim.mohammed.dev@gmail.com>
2025-06-30 18:24:25 +0300

    Refactor AddNoteCubit and NotesApp: implement BlocProvider for state management and enhance addNote method with error handling

--- lib/cubits/add_note_cubit.dart
+++ lib/cubits/add_note_cubit.dart
@@ -1,11 +1,18 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:notes_app/cubits/add_note_state.dart';
+import 'package:hive/hive.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/models/note_model.dart';
+import 'package:notes_app/cubits/add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitialState());
-  addNote(NoteModel noteModel)
-  {
-    
+  addNote(NoteModel note) {
+    try {
+      var noteBox = Hive.box<NoteModel>(kNotesBox);
+      noteBox.add(note);
+      emit(AddNoteSuccessState());
+    } catch (e) {
+      emit(AddNoteFailureState(errorMessage: e.toString()));
+    }
   }
 }
--- lib/notes_app.dart
+++ lib/notes_app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/add_note_cubit.dart';
 import 'package:notes_app/views/edit_note_view.dart';
 import 'package:notes_app/views/home_view.dart';
 
@@ -7,14 +9,17 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        HomeView.id: (context) => const HomeView(),
-        EditNoteView.id: (context) => const EditNoteView(),
-      },
-      initialRoute: HomeView.id,
-      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
-      debugShowCheckedModeBanner: false,
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (context) => AddNoteCubit())],
+      child: MaterialApp(
+        routes: {
+          HomeView.id: (context) => const HomeView(),
+          EditNoteView.id: (context) => const EditNoteView(),
+        },
+        initialRoute: HomeView.id,
+        theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
