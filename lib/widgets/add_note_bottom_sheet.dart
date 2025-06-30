f9f5f0c1dfc07726eb31ab160a3b39620e952412
1dev-Ebrahim <ebrahim.mohammed.dev@gmail.com>
2025-06-30 23:33:16 +0300

    Implement loading state and error handling in AddNoteBottomSheet; add modal_progress_hud_nsn dependency

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,13 +1,18 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:notes_app/cubits/add_note_cubit.dart';
+import 'package:notes_app/cubits/add_note_state.dart';
 import 'package:notes_app/widgets/add_note_form.dart';
-import 'package:notes_app/widgets/custom_elevated_button.dart';
-import 'package:notes_app/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // bool isLoading = false;
     return AnimatedPadding(
       duration: const Duration(milliseconds: 0),
       curve: Curves.ease,
@@ -16,8 +21,43 @@ class AddNoteBottomSheet extends StatelessWidget {
         right: 16.0,
         bottom: MediaQuery.of(context).viewInsets.bottom,
       ),
-      child: const AddNoteForm(),
+      child: BlocConsumer<AddNoteCubit, AddNoteState>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          if (state is AddNoteFailureState) {
+            log(state.errorMessage);
+            customSnackBar(
+              context,
+              contentText: 'Unexpected error occoured please try again',
+              backgroundColor: Colors.red,
+            );
+          } else if (state is AddNoteSuccessState) {
+            customSnackBar(
+              context,
+              contentText: 'Note has been added successfully',
+              backgroundColor: Colors.green,
+            );
+            Navigator.pop(context);
+          }
+          return ModalProgressHUD(
+            inAsyncCall: state is AddNoteLoadingState,
+            child: const AddNoteForm(),
+          );
+        },
+      ),
+    );
+  }
+
+  void customSnackBar(
+    BuildContext context, {
+    required String contentText,
+    Color? backgroundColor,
+  }) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        backgroundColor: backgroundColor,
+        content: Text(contentText, style: const TextStyle(color: Colors.white)),
+      ),
     );
   }
 }
-
