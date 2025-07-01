41f8f083738f47a29c14671bb0431a2a5eab3398
1dev-Ebrahim <ebrahim.mohammed.dev@gmail.com>
2025-07-01 22:28:54 +0300

    Refactor HomeView and NotesListView: integrate NotesCubit for state management and clean up code formatting

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/constants.dart';
+import 'package:notes_app/cubits/add_note_cubit/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/widgets/add_note_bottom_sheet.dart';
 import 'package:notes_app/widgets/custom_appbar.dart';
 import 'package:notes_app/widgets/notes_list_view.dart';
@@ -9,26 +11,31 @@ class HomeView extends StatelessWidget {
   static const String id = 'HomeView';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-            context: context,
-            builder: (context) => const AddNoteBottomSheet(),
-            showDragHandle: true,
-            isScrollControlled: true,
-          );
-        },
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
-        backgroundColor: kPrimaryColor,
-        child: const Icon(Icons.add, color: Colors.black),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+              context: context,
+              builder: (context) => const AddNoteBottomSheet(),
+              showDragHandle: true,
+              isScrollControlled: true,
+            );
+          },
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50),
+          ),
+          backgroundColor: kPrimaryColor,
+          child: const Icon(Icons.add, color: Colors.black),
+        ),
+        appBar: CustomAppBar(
+          title: 'Notes',
+          iconData: Icons.search,
+          onPressed: () {},
+        ),
+        body: const NotesListView(),
       ),
-      appBar: CustomAppBar(
-        title: 'Notes',
-        iconData: Icons.search,
-        onPressed: () {},
-      ),
-      body: const NotesListView(),
     );
   }
 }
